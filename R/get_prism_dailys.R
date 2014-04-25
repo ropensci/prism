7f7fafaf4c6272ab5ba297dcd1d9731515adf5a3
emhart <edmund.m.hart@gmail.com>
2014-04-24 23:58:48 -0700

    Added fxn for getting daily data.
    Also completed fxn for getting normals.

---
+++ R/get_prism_dailys.R
@@ -0,0 +1,53 @@
+#' Download daily prism data
+#' @description Download daily data from the prism project at 4km grid cell resolution for precipitation, mean, min and max temperature
+#' @param type The type of data to download, must be "ppt", "tmean", "tmin", or "tmax".
+#' @param minDate a valid iso-8601 (e.g. YYYY-MM-DD) date to start to download data.
+#' @param maxDate  a valid iso-8601 (e.g. YYYY-MM-DD) date to end downloading data.
+#' @param dates a vector of iso-8601 formatted dates to download data for, can also be a single date.
+#' @param keepZip if true, leave the downloaded zip files in your 'prism.path', if FALSE, they will be deleted
+#' @details Dates must be in the proper format or downloading will not work properly, you can either enter a date range via minDate and maxDate, or a vector of dates, but not both. You must make sure that you have set up a valid download directory.  This must be set as options(prism.path = "YOURPATH")
+#' @export
+get_prism_dailys <- function(type, minDate = NULL, maxDate =  NULL, dates = NULL, keepZip = TRUE){
+  
+  if(!is.null(dates) && !is.null(maxDate)){
+    stop("You can enter a date range or a vector of dates, but not both")
+  }
+  
+  if(is.null(dates)){
+    dates <- seq(as.Date(minDate),as.Date(maxDate),by="days")
+  }
+  
+  
+  ### parameter and error handling
+  
+  type <- match.arg(type, c("ppt","tmean","tmin","tmax"))
+ 
+  
+  
+    for(i in month){
+      files <- c(files,paste("PRISM_",type,"_30yr_normal_", res,"M2_",i,"_bil.zip",sep=""))
+    }
+
+  download_pb <- txtProgressBar(min = 0, max = length(dates), style = 3)
+  
+  base <- "ftp://prism.nacse.org/daily"
+
+  for(i in 1:length(dates)){
+    #parse date
+    dstring <- strsplit(as.character(dates[i]),"-")[[1]]
+    if(as.numeric(dstring[1]) < 1981){stop("You must ask for data after 1981")}
+    full_path <- paste(base,paste(type,dstring[1],sep="/"),sep="/")
+    fileName <- paste("PRISM_",type,"_stable_4kmD1_",paste(dstring,collapse=""),"_bil.zip",sep="")
+    outFile <- paste(options("prism.path"),fileName,sep="/")
+    
+    download.file(url = paste(full_path,fileName,sep="/"), destfile = outFile, method = "curl",quiet=T)
+    unzip(outFile, exdir = strsplit(outFile,".zip")[[1]] )   
+    
+    if(!keepZip){
+      file.remove(outFile)
+    }
+    setTxtProgressBar(download_pb, i)
+  }
+  
+  close(download_pb)
+}
--- R/get_prism_normals.R
+++ R/get_prism_normals.R
@@ -5,15 +5,16 @@
 #' @param month a numeric value for month, can be a numeric vector of months.
 #' @param annual if true download annual data
 #' @param all if true, download all data.  Be careful because this can take some time, these files can be 100's of megabytes.
+#' @param keepZip if true, leave the downloaded zip files in your 'prism.path', if FALSE, they will be deleted
 #' @details You must make sure that you have set up a valid download directory.  This must be set as options(prism.path = "YOURPATH")
 #' 
 #' @export
-get_prism_normal <- function(type, resolution, month =  NULL , annual =  FALSE, all = FALSE){
+get_prism_normals <- function(type, resolution, month =  NULL , annual =  FALSE, all = FALSE, keepZip = TRUE){
   
   ### parameter and error handling
   
-  type <- match.args(type, c("ppt","tmean","tmin","tmax"))
-  res<- match.args(resolution, c("4km","800m"))
+  type <- match.arg(type, c("ppt","tmean","tmin","tmax"))
+  res<- match.arg(resolution, c("4km","800m"))
   
   if(annual && all){
     stop("Annual data is included in all, both cannot be set to TRUE")
@@ -34,11 +35,14 @@ get_prism_normal <- function(type, resolution, month =  NULL , annual =  FALSE, 
   
   base <- "ftp://prism.nacse.org"
   full_path <- paste(base,paste("normals_",res,sep=""),type,"",sep="/")
-  
-  outFile <- paste(options("prism.path"),files,sep="/")
-  download.file(url = paste(full_path,files,sep=""), destfile = outFile, method = "curl")
+  for(i in files){
+  outFile <- paste(options("prism.path"),i,sep="/")
+  download.file(url = paste(full_path,i,sep=""), destfile = outFile, method = "curl",quiet=T)
   unzip(outFile, exdir = strsplit(outFile,".zip")[[1]] )  
-  
+  if(!keepZip){
+    file.remove(outFile)
+  }
+  }
   
 }
 
