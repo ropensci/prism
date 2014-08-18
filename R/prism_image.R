c190fd9973ee42889457a12052fc1873db492dc0
Ted Hart <edmund.m.hart@gmail.com>
2014-08-17 22:00:53 -0600

    completed slicing plots close #10, added fxn for md extraction, and enhanced listing capabilities.

--- R/prism_image.R
+++ R/prism_image.R
@@ -4,6 +4,7 @@
 #' @param col the color pattern to use.  The default is heat, the other valid option is "redblue"
 #' @details This is meant for rapid vizualization, but more detailed plots will require other methods
 #' @examples \dontrun{
+#' get_prism_dailys(type="tmean", minDate = "2013-06-01", maxDate = "2013-06-14", keepZip=F)
 #' image_prism(ls_prism_data()[1])
 #' }
 #' @export
--- R/prism_slice.R
+++ R/prism_slice.R
@@ -1,20 +1,48 @@
+#' Plot a slice of a raster stack
+#' @description This function will plot a slice of data at a single point location from a list of prism files 
+#' @param location a vector of a single location in the form of long,lat
+#' @param prismfile a vector of output from ls_prism_data() giving a list of prism files to extract data from and plot
+#' @return a ggplot2 plot of the requested slice
+#' @details the list of prism files should be from a continuous data set. Otherwise the plot will look erratic and incorrect.
+#' @examples \dontrun{
+#' ### Assumes you have a clean prism directory
+#' get_prism_dailys(type="tmean", minDate = "2013-06-01", maxDate = "2013-06-14", keepZip=F)
+#' p <- prism_slice(c(-73.2119,44.4758),ls_prism_data())
+#' print(p)
+#' }
+#' @export
+
 
 prism_slice <- function(location,prismfile){
 
   prismfilexml <- paste(options("prism.path")[[1]],"/",prismfile,"/",prismfile,".xml",sep="")
   prismfilerast <- paste(options("prism.path")[[1]],"/",prismfile,"/",prismfile,".bil",sep="")
   
-  dates <- sapply(prismfilexml,md_date)
-  pstack <- prism_stack(ls_prism_data())
+  meta_d <- sapply(prismfilexml,prism_md,simplify=F)
+  pstack <- prism_stack(prismfile)
   data <- unlist(extract(pstack,matrix(location,nrow=1),buffer=10))
   data <- as.data.frame(data)
-  data$date <- as.POSIXct(dates,format="%Y%m%d")
-  ggplot(data,aes(x=date,y=data))+geom_path()+geom_point()
+  data$date <- unlist(lapply(meta_d,function(x){return(x[1])}))
+  data$date <- as.POSIXct(data$date,format="%Y%m%d")
+  ## Re order
+  data <- data[order(data$date),]
+  ifelse(grepl("temp",meta_d[[1]][3]),u <- "(C)", u <- "(mm)")
   
   
-md_date <- function(f){
+  out <- ggplot(data,aes(x=date,y=data))+geom_path()+geom_point()+xlab("Date") + ylab(paste(meta_d[[1]][3],u,collapse=""))
+  
+  return(out)
+}
+
+#' Extract select prism metadat
+#' @description used to extract some prism metadata used in other fxns
+#' @export
+
+prism_md <- function(f){
   m <- xmlParse(f)
   m <- xmlToList(m)
-  out <- m$idinfo$timeperd$timeinfo$rngdates$begdate
-  return(out)
+  date <- m$idinfo$timeperd$timeinfo$rngdates$begdate
+  prod_title <- m$idinfo$citation$citeinfo$title 
+  prod_name <- strsplit(m$eainfo$detailed$attr$attrlabl,'\\(')[[1]][1]
+  return(c(date,prod_title,prod_name))
 }
