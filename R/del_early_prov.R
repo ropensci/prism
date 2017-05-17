3840e534ad1acc99f07779b74914fbe3d28b4bed
Kendon Bell <kendonB@users.noreply.github.com>
2017-05-16 20:49:40 -0700

    added some robustness to del_early_prov

--- R/del_early_prov.R
+++ R/del_early_prov.R
@@ -5,31 +5,47 @@
 #' @importFrom dplyr bind_rows
 #' @return NULL
 #' @export
-del_early_prov <- function(type, minDate = NULL, maxDate = NULL, dates = NULL){
+del_early_prov <- function (type, minDate = NULL, maxDate = NULL, dates = NULL) 
+{
   path_check()
-  dates <- prism:::gen_dates(minDate = minDate, maxDate = maxDate, dates = dates)
-  md <- prism:::get_metadata(type = type, dates = dates)
-  md <- lapply(md, function(x){
-    names(x) <- stringr::str_replace(names(x), "_DATASET", "")
+  dates <- gen_dates(minDate = minDate, maxDate = maxDate, 
+                             dates = dates)
+  md <- get_metadata(type = type, dates = dates)
+  md <- lapply(md, function(x) {
+    names(x) <- stringr::str_replace(names(x), "_DATASET", 
+                                     "")
     x
   })
   mddf <- dplyr::bind_rows(md)
   mddf$dates_str <- stringr::str_extract(mddf$PRISM_FILENAME, 
                                          "[0-9]{8}")
-  
   duplicates <- mddf$dates_str[duplicated(mddf$dates_str)]
-  for(dup in duplicates){
-    dups <- mddf[mddf$dates_str == dup,]
+  for (dup in duplicates) {
+    dups <- mddf[mddf$dates_str == dup, ]
     dups$dates_num <- as.numeric(dups$dates_str)
-    
-    # This assumes that the files are in order of date modified
-    to_delete <- 1:(NROW(dups) - 1)
-    
-    unlink(dups$folder_path[to_delete], recursive = TRUE)
-    # Check if the folder was deleted.
-    if(file.exists(file.path(dups$file_path[to_delete], ".."))){
-      warning(paste0("Unable to remove folder ", file.path(dups$file_path[to_delete], ".."),
-                     ". Check permissions."))
+    dups$priority <- sapply(dups$folder_path, function(x){
+      if(stringr::str_detect(x, "stable")){
+        return(1)
+      } else if (stringr::str_detect(x, "provisional")){
+        return(2)
+      } else if (stringr::str_detect(x, "early")){
+        return(3)
+      }
+    })
+    dups$to_delete <- dups$priority > min(dups$priority)
+    if(sum(!dups$to_delete) > 1){
+      # Here, there is more than one of the highest priority
+      # Just assume the later one is newer
+      dups$new_to_delete <- dups$to_delete
+      dups$new_to_delete[!dups$to_delete] <- TRUE
+      dups$new_to_delete[!dups$to_delete][length(dups$to_delete[!dups$to_delete])] <- FALSE
+      dups$to_delete <- dups$new_to_delete
+    }
+    unlink(dups$folder_path[dups$to_delete], recursive = TRUE)
+    if (file.exists(file.path(dups$file_path[dups$to_delete], 
+                              ".."))) {
+      warning(paste0("Unable to remove folder ", file.path(dups$file_path[dups$to_delete], 
+                                                           ".."), ". Check permissions."))
     }
   }
   NULL
