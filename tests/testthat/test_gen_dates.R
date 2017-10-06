8b4a9eca5e1057d4bf3f5337059eb1276ea83f0f
Alan Butler <rabutler@usbr.gov>
2017-10-06 10:31:09 -0600

    more `gen_dates()` tests

--- tests/testthat/test_gen_dates.R
+++ tests/testthat/test_gen_dates.R
@@ -1,7 +1,32 @@
 
 context("Test gen_dates")
+d1 <- "2016-01-01"
+test_that("gen_dates returns expected data", {
+  expect_equal(
+    class(gen_dates(minDate = "2016-01-01", maxDate = "2016-01-01", dates = NULL)),
+    "Date"
+  )
+  expect_length(gen_dates(minDate = NULL, maxDate = NULL, dates = d1), 1)
+  expect_length(
+    gen_dates(minDate = NULL, maxDate = NULL, dates = c(d1,"2016-03-03", "2015-04-25")), 
+    3
+  )
+  expect_length(
+    gen_dates(minDate = "2015-01-01", maxDate = "2015-12-31", dates = NULL), 
+    365
+  )
+  expect_length(gen_dates(minDate = d1, maxDate = "2016-12-31", dates = NULL), 366)
+  expect_equal(
+    gen_dates(minDate = d1, maxDate = d1, dates = NULL),
+    gen_dates(minDate = NULL, maxDate = NULL, dates = d1)
+  )
+  expect_equal(
+    gen_dates(minDate = NULL, maxDate = NULL, dates = d1),
+    gen_dates(minDate = NULL, maxDate = NULL, dates = as.Date(d1))  
+  )
+})
 
-test_that("errors work properly", {
+test_that("gen_dates errors properly", {
   expect_error(gen_dates(minDate = "2016-01-01", maxDate = NULL, dates = "2016-01-02"),
                "You can enter a date range or a vector of dates, but not both")
   expect_error(gen_dates(minDate = NULL, maxDate = "2016-01-01", dates = "2016-01-02"),
