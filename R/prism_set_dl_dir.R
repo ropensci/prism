4c6de3af66ffd49fdb99244b3f57d18456aa323b
Alan Butler <rabutler@usbr.gov>
2019-12-31 18:30:18 -0700

    suppress warning message in call to normalizePath()

--- R/prism_set_dl_dir.R
+++ R/prism_set_dl_dir.R
@@ -12,7 +12,7 @@
 prism_set_dl_dir <- function(path, create = TRUE)
 {
   # Check if path exists, if it doesn't then create it
-  path <- normalizePath(path)
+  path <- normalizePath(path, mustWork = FALSE)
   if (!dir.exists(file.path(path)) & create) {
     message("creating ", path)
     dir.create(path, recursive = TRUE)
