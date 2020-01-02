1b5ffd9554e8c546f9f55c8d88c901711ff17d96
Alan Butler <rabutler@usbr.gov>
2020-01-02 09:57:33 -0700

    fixing minor bugs in `get_prism_normals()`
    1. if `months == NULL & annual == FALSE`, an error is raised before trying to download no data
    2. if `months %in% 1:12 & annual == TRUE` then monthly and annual data are downloaded
    3. added tests to check these errors
    4. minor documentation updates
    
    closes #77

--- R/get_prism_normals.R
+++ R/get_prism_normals.R
@@ -1,36 +1,63 @@
 #' Download data for 30 year normals of climate variables
-#' @description Download data from the prism project for 30 year normals at 4km or 800m grid cell resolution for precipitation, mean, min and max temperature
-#' @inheritParams get_prism_dailys
-#' @param resolution The spatial resolution of the data, must be either "4km" or "800m".
+#' 
+#' @description Download data from the prism project for 30 year normals at 4km 
+#'   or 800m grid cell resolution for precipitation, mean, min and max 
+#'   temperature
+#' 
+#' @inheritParams get_prism_annual
+#' 
+#' @param resolution The spatial resolution of the data, must be either "4km" 
+#'   or "800m".
+#' 
 #' @param mon a numeric value for month, can be a numeric vector of months.
-#' @param annual if TRUE download annual data
-#' @param keepZip if TRUE, leave the downloaded zip files in your 'prism.path', if FALSE, they will be deleted
-#' @details You must make sure that you have set up a valid download directory.  This must be set as options(prism.path = "YOURPATH")
+#' 
+#' @param annual if `TRUE` download annual data
+#' 
+#' @details You must make sure that you have set up a valid download directory.  
+#' This must be set as options(prism.path = "YOURPATH"). If `mon` is specified
+#' and `annual` is `TRUE`, then monthly and annual data will be downloaded.
+#' 
 #' @examples \dontrun{
-#' ### Get 30 year normal values for rainfall
+#' ### Get 30 year normal values for January rainfall
 #' get_prism_normals(type="ppt",resolution = "4km",mon = 1, keepZip=FALSE)
 #' 
 #' }
+#' 
 #' @export
-get_prism_normals <- function(type, resolution, mon =  NULL , annual =  FALSE,  keepZip = TRUE){
+get_prism_normals <- function(type, resolution, mon = NULL, annual = FALSE,  
+                              keepZip = TRUE)
+{
   ### parameter and error handling
   prism_check_dl_dir()
   type <- match.arg(type, prism_vars())
-  resolution<- match.arg(resolution, c("4km","800m"))
+  resolution <- match.arg(resolution, c("4km","800m"))
   
+  if (is.null(mon) & !annual) {
+    stop(
+      "`mon` is `NULL` and `annual` is `FALSE`.\n",
+      "Specify either monthly or/and annual data to download."
+    )
+  }
+  
+  call_mon <- c()
   if(!is.null(mon)){
     if(any(mon < 1 | mon > 12)) {
       stop("You must enter a month between 1 and 12")
     }
-    mon <- mon_to_string(mon)
-    match_list <- mon
-  } else if(annual){
-    match_list <- "annual"
-    mon <- "14"
+    call_mon <- mon_to_string(mon)
+  } 
+  
+  if (annual) {
+    call_mon <- c(call_mon, "14")
   }
-  
-  uris <- sapply(mon,function(x){paste("http://services.nacse.org/prism/data/public/normals",resolution,type,x,sep="/")})
-  
+ 
+  uris <- sapply(call_mon, function(x) {
+    paste(
+      "http://services.nacse.org/prism/data/public/normals",
+      resolution, type, x,
+      sep="/"
+    )
+  })
 
   mpb <- txtProgressBar(min = 0, max =length(uris), style = 3)
  
@@ -40,8 +67,5 @@ get_prism_normals <- function(type, resolution, mon =  NULL , annual =  FALSE,  
     
   }
   
-  
   close(mpb)
-  
-  
 }
