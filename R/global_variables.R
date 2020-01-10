326b97a128182a39577aaf6de507723fd1aea051
Alan Butler <rabutler@usbr.gov>
2020-01-10 14:34:41 -0700

    fixing the note on global variables

--- R/global_variables.R
+++ R/global_variables.R
@@ -4,6 +4,6 @@ if(getRversion() >= "2.15.1"){
   # gloabl variables from get_prism_station_md()
   gv <- c("ELEV(m)", "elevation", "Elevation(m)", "ID", "LAT", "latitude",
           "Latitude", "LON", "longitude", "Longitude", "name", "Name", "NAME",
-          "network", "Network", "station", "Station", "stnid")
+          "network", "Network", "station", "Station", "stnid", "station_id")
   utils::globalVariables(gv)
 }
