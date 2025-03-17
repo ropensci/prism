ropensci/prism

commit 0ac0483e21912ac8d23d1ed6d605374763c04fc5

remove all uses of sapply()
#121

R/get_prism_monthlys.R
@@ -49,14 +49,7 @@ get_prism_monthlys <- function(type, years, mon = 1:12, keepZip = TRUE,
   }
   
   if (length(pre_1981)) {
-    uris_pre81 <- sapply(
-      pre_1981,
-      function(x) {
-        paste(
-          service, type, x, sep = "/"
-        )
-      }
-    )
+    uris_pre81 <- gen_prism_url(pre_1981, type, service)
   }
 
   if (length(post_1981)) {  
@@ -66,14 +59,7 @@ get_prism_monthlys <- function(type, years, mon = 1:12, keepZip = TRUE,
       function(x) {paste(x[1], x[2], sep="")}
     )
     
-    uris_post81 <- sapply(
-      uri_dates_post81,
-      function(x) {
-        paste(
-          service, type, x, sep="/"
-        )
-      }
-    )
+    uris_pre81 <- gen_prism_url(uri_dates_post81, type, service)
   }
     
   download_pb <- txtProgressBar(
@@ -93,7 +79,7 @@ get_prism_monthlys <- function(type, years, mon = 1:12, keepZip = TRUE,
   }
     
   counter <- length(uris_post81)+1
-   
+
   ### Handle pre 1981 files
   if (length(uris_pre81) > 0) {
   
@@ -126,9 +112,7 @@ get_prism_monthlys <- function(type, years, mon = 1:12, keepZip = TRUE,
           monstr <- mon_to_string(mon)
         }
         
-        to_split <-   sapply(monstr, function(x) {
-          gsub(pattern = "_all", replacement = x, x = pre_files[k])
-        })
+        to_split <- stringr::str_replace(pre_files[k], "_all", monstr)
         
         process_zip(pre_files[k], to_split)
       }

R/get_prism_annual.R
@@ -82,25 +82,11 @@ get_prism_annual <- function(type, years, keepZip = TRUE,
   }  
   
   if (length(pre_1981)) {
-    uris_pre81 <- sapply(
-      pre_1981,
-      function(x) {
-        paste(
-          service, type, x, sep = "/"
-        )
-      }
-    )
+    uris_pre81 <- gen_prism_url(pre_1981, type, service)
   }
   
   if (length(post_1981)) {  
-    uris_post81 <- sapply(
-      post_1981,
-      function(x) {
-        paste(
-          service, type, x, sep = "/"
-        )
-      }
-    )
+    uris_post81 <- gen_prism_url(post_1981, type, service) 
   }
   
   download_pb <- txtProgressBar(

R/get_prism_dailys.R
@@ -94,21 +94,18 @@ get_prism_dailys <- function(type, minDate = NULL, maxDate =  NULL,
   }
 
   uri_dates <- gsub(pattern = "-",replacement = "",dates)
-  uris <- sapply(uri_dates, function(x) {
-    paste(
-      service, type, x, 
-      sep = "/"
-    )
-  })
-  
+  uris <- gen_prism_url(uri_dates, type, service)
+ 
   if(check == "internal"){
     x <- httr::HEAD(uris[1])
     fn <- x$headers$`content-disposition`
     fn <- regmatches(fn,regexpr('\\"[a-zA-Z0-9_\\.]+',fn))
     fn <- substr(fn,2,nchar((fn)))
     fn <- gsub("provisional|early", "stable", fn)
-    file_names <- sapply(uri_dates, function(x) 
-      gsub("[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]", x, fn)
+    file_names <- stringr::str_replace(
+      fn, 
+      "[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]", 
+      uri_dates
     )
     to_download_lgl <- prism_check(file_names, lgl = TRUE)
     uris <- uris[to_download_lgl]

R/get_prism_normals.R
@@ -61,13 +61,8 @@ get_prism_normals <- function(type, resolution, mon = NULL, annual = FALSE,
     call_mon <- c(call_mon, "14")
   }
  
-  uris <- sapply(call_mon, function(x) {
-    paste(
-      "http://services.nacse.org/prism/data/public/normals",
-      resolution, type, x,
-      sep="/"
-    )
-  })
+  stemp <- "http://services.nacse.org/prism/data/public/normals"
+  uris <- gen_prism_url(call_mon, type, stemp, resolution) 
 
   mpb <- txtProgressBar(min = 0, max =length(uris), style = 3)
  

R/utility_functions.R
@@ -24,10 +24,11 @@ mon_to_string <- function(month)
 
 prism_not_downloaded <- function(zipfiles, lgl = FALSE, pre81_months = NULL)
 {
-  file_bases <- unlist(sapply(zipfiles, strsplit, split=".zip"))
-  which_downloaded <- sapply(
+  file_bases <- stringr::str_remove(zipfiles, '.zip')
+  which_downloaded <- vapply(
     file_bases, 
     find_prism_file, 
+    FUN.VALUE = logical(1),
     pre81_months = pre81_months
   )
   
@@ -35,6 +36,17 @@ prism_not_downloaded <- function(zipfiles, lgl = FALSE, pre81_months = NULL)
     return(!which_downloaded)
   } else {
     return(zipfiles[!which_downloaded])    
+  }
+}
+
+# x can be vector of years/ months/datee
+# will return value that is same length as x
+gen_prism_url <- function(x, type, service, resolution = NULL) 
+{
+  if (is.null(resolution)) {
+    paste(service, type, x, sep = "/")
+  } else {
+    paste(service, resolution, type, x, sep = "/")
   }
 }
 
@@ -128,25 +140,24 @@ find_prism_file <- function(base_file, pre81_months)
 #' @noRd
 process_zip <- function(pfile, name) 
 {
-  tmpwd <- list.files(paste(options("prism.path")[[1]], pfile, sep="/"))
+  tmpwd <- list.files(file.path(prism_get_dl_dir(), pfile))
   
   # Remove all.xml file
-  file.remove(paste(
-    options("prism.path")[[1]], 
+  file.remove(file.path(
+    prism_get_dl_dir(), 
     pfile, 
-    grep("all", tmpwd, value = TRUE), 
-    sep="/"
+    grep("all", tmpwd, value = TRUE)
   ))
   
   # Get new list of files after removing all.xml
-  tmpwd <- list.files(paste(options("prism.path")[[1]], pfile, sep="/"))
+  tmpwd <- list.files(file.path(prism_get_dl_dir(), pfile))
   
   fstrip <- strsplit(tmpwd, "\\.")
   fstrip <- unlist(lapply(fstrip, function(x) return(x[1])))
   unames <- unique(fstrip)
   unames <- unames[unames %in% name]
   for(j in 1:length(unames)){
-    newdir <- paste(options("prism.path")[[1]], unames[j], sep="/")
+    newdir <- file.path(prism_get_dl_dir(), unames[j])
     tryCatch(
       dir.create(newdir), 
       error = function(e) e,
@@ -156,23 +167,22 @@ process_zip <- function(pfile, name)
     )
     
     f2copy <- grep(unames[j], tmpwd, value = TRUE)
-    sapply(f2copy, function(x){
-      file.copy(from = paste(options("prism.path")[[1]], pfile, x, sep="/"),
-                to = paste(newdir, x, sep="/"))
-    })
-    sapply(f2copy, function(x){
-      file.remove(paste(options("prism.path")[[1]], pfile, x, sep="/"))
-    })
+    
+    file.copy(
+      from = file.path(prism_get_dl_dir(), pfile, f2copy),
+      to = file.path(newdir, f2copy)
+    )
+    
+    file.remove(file.path(prism_get_dl_dir(), pfile, f2copy))
     # We lose all our metadata, so we need to rewrite it
   }
   # Remove all files so the directory can be created.
   # Update file list
-  tmpwd <- list.files(paste(options("prism.path")[[1]], pfile, sep="/"))
+  tmpwd <- list.files(file.path(prism_get_dl_dir(), pfile))
   ## Now loop delete them all
-  sapply(tmpwd, function(x){
-    file.remove(paste(options("prism.path")[[1]], pfile, x, sep="/"))
-  })
-  unlink(paste(options("prism.path")[[1]], pfile, sep="/"), recursive = TRUE)
+  file.remove(file.path(prism_get_dl_dir(), pfile, tmpwd))
+  
+  unlink(file.path(prism_get_dl_dir(), pfile), recursive = TRUE)
 }
 
 #' Checks to see if the dates (days) specified are within the available Prism 

commit 0f2943e8faea2e60ab229b4877e5e7e1cd6fd39d

fixed bug in get_prism_monthlys() introduced by 0ac0483

R/get_prism_monthlys.R
@@ -17,7 +17,6 @@ get_prism_monthlys <- function(type, years, mon = 1:12, keepZip = TRUE,
                                keep_pre81_months = TRUE, service = NULL)
 {
   ### parameter and error handling
-
   prism_check_dl_dir()
   type <- match.arg(type, prism_vars())
  
@@ -45,7 +44,7 @@ get_prism_monthlys <- function(type, years, mon = 1:12, keepZip = TRUE,
   uris_post81 <- vector()
   
   if (is.null(service)) {
-	service <- "http://services.nacse.org/prism/data/public/4km"
+	  service <- "http://services.nacse.org/prism/data/public/4km"
   }
   
   if (length(pre_1981)) {
@@ -59,7 +58,7 @@ get_prism_monthlys <- function(type, years, mon = 1:12, keepZip = TRUE,
       function(x) {paste(x[1], x[2], sep="")}
     )
     
-    uris_pre81 <- gen_prism_url(uri_dates_post81, type, service)
+    uris_post81 <- gen_prism_url(uri_dates_post81, type, service)
   }
     
   download_pb <- txtProgressBar(