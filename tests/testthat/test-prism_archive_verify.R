1d2fff24a89671147528018d32e295bcb2b157f6
rl627 <45013044+ran-codes@users.noreply.github.com>
2025-08-08 15:05:03 -0400

    updated test-prism_archive_verify.R for resolutionr equirements and web service v2 expected urls from folder_to_url() #19

--- tests/testthat/test-prism_archive_verify.R
+++ tests/testthat/test-prism_archive_verify.R
@@ -4,13 +4,13 @@
 
 test_that("corrupt file is found", {
   expect_equal(
-    expect_warning(prism_archive_verify("ppt", "daily", 1991:2012, download_corrupt = FALSE)),
+    expect_warning(prism_archive_verify("ppt", "daily", 1991:2012, download_corrupt = FALSE, resolution = '4km')),
     "PRISM_ppt_stable_4kmD2_20120101_bil"
   )
 })
 
 test_that("returns true when all valid", {
-  expect_true(prism_archive_verify("tmin", "daily"))
+  expect_true(prism_archive_verify("tmin", "daily", resolution = '4km'))
 })
 
 # check that url is correctly recreated
@@ -18,16 +18,16 @@ test_that("returns true when all valid", {
 test_that("url is created correctly", {
   expect_equal(
     prism:::folder_to_url("PRISM_ppt_stable_4kmD2_20120101_bil"),
-    "http://services.nacse.org/prism/data/public/4km/ppt/20120101"
+    "https://services.nacse.org/prism/data/get/us/4km/ppt/20120101?format=bil"
   )
   
   ff <- c("PRISM_ppt_stable_4kmD2_20120101_bil", 
           "PRISM_ppt_30yr_normal_800mM2_02_bil",
           "PRISM_tmax_30yr_normal_4kmM2_annual_bil")
   f2 <- c(
-    "http://services.nacse.org/prism/data/public/4km/ppt/20120101",
-    "http://services.nacse.org/prism/data/public/normals/800m/ppt/02",
-    "http://services.nacse.org/prism/data/public/normals/4km/tmax/14"
+    "https://services.nacse.org/prism/data/get/us/4km/ppt/20120101?format=bil",
+    "https://data.prism.oregonstate.edu/normals_bil/monthly/4km/ppt/PRISM_ppt_30yr_normal_4kmM4_02_bil.zip",
+    "https://data.prism.oregonstate.edu/normals_bil/monthly/4km/tmax/PRISM_tmax_30yr_normal_4kmM5_annual_bil.zip"
   )
   
   expect_equal(prism:::folder_to_url(ff), f2)
