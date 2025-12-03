e6f706d2f5d63be8fdc0672f41167af105e32f0d
Alan Butler <rabutler@usbr.gov>
2025-12-03 10:43:34 -0700

    cleaning up tests for deprecated functions
    #117

--- tests/testthat/test-prism_archive_ls.R
+++ tests/testthat/test-prism_archive_ls.R
@@ -3,17 +3,6 @@ n_files <- 10
 
 test_that("Directory listings work",{
   skip_on_cran()
-
+  
   expect_warning(expect_length(x <- prism_archive_ls(), n_files))
-  
-  df <- data.frame(
-    files = x,
-    abs_path = pd_to_file(x),
-    product_name = pd_get_name(x),
-    stringsAsFactors = FALSE
-  )
-  
-  y$abs_path <- normalizePath(y$abs_path)
-  
-  expect_identical(df, y)
 }) 
