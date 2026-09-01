c41df563ff472ba4311e5da26ab26bc1f7f27779
Alan Butler <rabutler@usbr.gov>
2026-09-01 11:34:37 -0600

    for this release, default download format will be .bil

--- R/zzz.R
+++ R/zzz.R
@@ -4,9 +4,10 @@
 .onAttach <- function(libname, pkgname){
   packageStartupMessage(
     "Be sure to set the download folder using `prism_set_dl_dir()`.",
-    "\nBy default, prism data are downloaded in GeoTIFF format. ",
-    "See ?prism_set_format for more details."
+    "\nBy default, prism data are downloaded in bil format. ",
+    "See ?prism_set_format for more details.\n",
+    "This will be changed to GeoTiff in the next release."
   )
   
-  options(prism.format = "geotiff")
+  options(prism.format = "bil")
 }
