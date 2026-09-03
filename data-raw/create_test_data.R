876db7d0d041886afe469ceefe4dd84969e80e4c
Alan Butler <rabutler@usbr.gov>
2026-09-03 12:13:52 -0600

    changed the bulk of the test data
    - new api format
    - include bil, nc, tif, and asc data
    - trimmed it to be only Boulder County
    
    #155

--- data-raw/create_test_data.R
+++ data-raw/create_test_data.R
@@ -2,83 +2,179 @@
 # so will read in the PRISM data, trim to smaller geographic area, and replace
 # original files in tests/testthat/prism_test
 
-library(raster)
+library(terra)
+library(sf)
 library(prism)
 library(maps)
 
+# bil - annual tmax
+# asci - monthly tmean
+# nc - daily tmin
+# geotiff - annual precip * 3
+# geotiff - 30-year normal annual precip
+
+
+# download data
+bil_dir <- file.path("data-raw/prism/bil")
+asc_dir <- file.path("data-raw/prism/asc")
+nc_dir <- file.path("data-raw/prism/nc")
+tif_dir <- file.path("data-raw/prism/tif")
+dl_dir <- file.path("data-raw/prism")
+
+prism_set_dl_dir(bil_dir)
+prism_set_format('bil')
+get_prism_annual("tmax", 2025)
+
+prism_set_dl_dir(asc_dir)
+prism_set_format('asc')
+get_prism_monthlys('tmean', 2025, 7)
+
+prism_set_dl_dir(nc_dir)
+prism_set_format('nc')
+get_prism_dailys('tmin', dates = "2025-07-04")
+
+prism_set_dl_dir(tif_dir)
+prism_set_format('geotiff')
+get_prism_annual('ppt', 2023:2025)
+get_prism_normals('ppt', '4km', annual = TRUE)
+
 # unzip data -------------------------------------
-# the dl directory is the test file directory
-dl_dir <- file.path("data-raw/prism/")
-#prism_set_dl_dir(dl_dir)
-
 # need to unzip all the zip files, then delete the unzipped folder when exiting
-avail_ppt <- paste0(
-  "PRISM_ppt_stable_4kmD2_",
-  c("19910101", "20110101"),
-  "_bil"
-)
-avail_tmin <- paste0(
-  "PRISM_tmin_stable_4kmD2_",
-  c("19810101", "20110615"),
-  "_bil"
-)
-
-avail_tdmean <- paste0("PRISM_tdmean_stable_4kmM3_2005", 11:12, "_bil")
-
-avail_vpdmin <- "PRISM_vpdmin_30yr_normal_4kmM4_annual_bil"
-
-avail_daily_normal <- "PRISM_ppt_30yr_normal_4kmD1_0301_bil"
-
-all_avail <- c(avail_tmin, avail_ppt, avail_tdmean, avail_vpdmin)
-
-all_avail <- avail_daily_normal
+all_avail <- list.files(dl_dir, recursive = TRUE)
 
 for (ff in all_avail) {
+  tmp_folder <- stringr::str_remove(ff, "\\.zip$")
+  
   utils::unzip(
-    file.path(dl_dir, paste0(ff, ".zip")), 
-    exdir = file.path(dl_dir, ff)
+    file.path("data-raw/prism", ff), 
+    exdir = file.path(dl_dir, tmp_folder)
   )
 }
 
-# subset of states -------------------------------
-ss <- sf::st_as_sf(maps::map(
-  "state", 
-  c("california"),
-  plot = FALSE
-)) 
 
-ss <- raster::extent(ss)
+# crop/trim the prism data
+# Boulder County boundary ------------------------------------------------
+# `maps::map("county")` identifies counties as "state,county".
 
-# read prism data ---------------------
-for (ff in all_avail) {
-  print(ff)
-  bil_file <- file.path("data-raw/prism", ff, paste0(ff,".bil"))
+boulder_county <- sf::st_as_sf(
+  maps::map(
+    database = "county",
+    regions = "colorado,boulder",
+    fill = TRUE,
+    plot = FALSE
+  )
+)
+
+# `maps` coordinates are longitude/latitude.
+sf::st_crs(boulder_county) <- "EPSG:4326"
+
+# Crop and save data -------------------------------------------------
+# `all_avail` should be your vector of PRISM-data folder names.
+# Example:
+# all_avail <- c(
+#   "prism_ppt_us_25m_201001",
+#   "prism_tmax_us_25m_20020703"
+# )
+
+write_prism_fixture <- function(r, filename, format) {
+  switch(
+    format,
+    bil = terra::writeRaster(
+      r,
+      filename = filename,
+      filetype = "EHdr",
+      overwrite = TRUE
+    ),
+    
+    asc = terra::writeRaster(
+      r,
+      filename = filename,
+      filetype = "AAIGrid",
+      overwrite = TRUE
+    ),
+    
+    tif = terra::writeRaster(
+      r,
+      filename = filename,
+      filetype = "GTiff",
+      overwrite = TRUE
+    ),
+    
+    nc = terra::writeCDF(
+      r,
+      filename = filename,
+      overwrite = TRUE
+    ),
+    
+    stop(
+      "Unsupported output format: `", format, "`.",
+      call. = FALSE
+    )
+  )
+}
+
+all_avail <- stringr::str_remove(all_avail, ".zip")
+
+for (ff in all_avail[4:7]) {
+  message("Cropping: ", ff)
   
-  f1 <- raster::raster(bil_file)
+  fext <- stringr::str_remove(ff, "/.*$")
+  tmp_folder <- stringr::str_remove(ff, ".*/")
   
-  # crop
-  f1 <- raster::crop(f1, ss)
+  bil_file <- file.path(
+    "data-raw",
+    "prism",
+    ff,
+    paste0(tmp_folder, ".", fext)
+  )
   
-  # save file --------------------
-  writeRaster(f1, bil_file, overwrite = TRUE)
+  # Read the real PRISM BIL raster.
+  r <- terra::rast(bil_file)
+  
+  # Reproject Boulder County to the raster CRS.
+  boulder_vect <- terra::project(
+    terra::vect(boulder_county),
+    terra::crs(r)
+  )
+  
+  # Crop to the county extent, then set cells outside the county to NA.
+  r_small <- terra::crop(r, boulder_vect)
+  r_small <- terra::mask(r_small, boulder_vect)
+  
+  # `EHdr` writes the BIL payload and the accompanying .hdr file.
+  write_prism_fixture(
+    r_small,
+    filename = bil_file,
+    format = fext
+  )
+  
+  r_check <- terra::rast(bil_file)
+  
+  stopifnot(
+    terra::ncell(r_check) > 0L,
+    any(!is.na(terra::values(r_check, mat = FALSE)))
+  )
 }
 
 # zip new files ------------------
 orig_wd <- getwd()
 on.exit(setwd(orig_wd))
 
-for (ff in all_avail) {
+for (ff in all_avail[2:7]) {
+  fext <- stringr::str_remove(ff, "/.*$")
+  tmp_folder <- stringr::str_remove(ff, ".*/")
+  
   setwd(file.path(dl_dir, ff))
   
   all_files <- list.files(".")
-  tmp_zip <- paste0(ff, ".zip")
+  tmp_zip <- paste0(tmp_folder, ".zip")
   
   zip(tmp_zip, all_files)
   
   setwd(orig_wd)
   file.copy(
     file.path(dl_dir, ff, tmp_zip), 
-    file.path("data-raw/small_prism", tmp_zip)
+    file.path("data-raw/small_prism", fext, tmp_zip)
   )
 }
 
--- tests/testthat/prism_test/README.md
+++ tests/testthat/prism_test/README.md
@@ -1,4 +1,4 @@
 ppt - 2012-01-01 is corrupt/bad data in the bil file. (It was intentionally corrupted to test one of the functions.)
 
-See data-raw/ for code that takes full .bil data and trims it to only California
+See data-raw/ for code that takes full .bil data and trims it to only Boulder County
 so that the overall file is small enough to work with CRAN policies
